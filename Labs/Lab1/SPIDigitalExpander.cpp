099948b582022b6ee46d5ded30baa13dfe61500a
kbeck8 <kbeck8@gatech.edu>
2018-08-29 09:39:46 -0400

    Added part 7

---
+++ Labs/Lab1/SPIDigitalExpander.cpp
@@ -0,0 +1,16 @@
+//--------------------------------------------------------------------------
+// 1.7 Expanding Digital I/O ports and SP
+//
+// Read a switch input and control an LED using an SPI serial bus I/O port expander IC.
+//
+// Harrison Statham
+//
+
+
+#include "SPIDigitalExpander.hpp"
+
+namespace Lab1 {
+
+
+
+}
